d797a8c97beeed98ae675a72460f7d70de285f2c
Miguel Angel Martinez Pazos <92331020+MPMiguel@users.noreply.github.com>
2023-07-19 16:57:00 +0000

    Excusa centered

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ let generarExcusa = () => {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
-  let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
   let whoIndex = Math.floor(Math.random() * who.length);
   let actionIndex = Math.floor(Math.random() * action.length);
